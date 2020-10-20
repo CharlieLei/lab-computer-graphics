570dd84b4cd8fed4834c04397a4345b8611cf662
charlielei <lei_cheng2008@126.com>
2020-10-21 01:04:56 +0800

    完成地形坐标

--- A/6_terrainengine/Terrain.h
+++ A/6_terrainengine/Terrain.h
@@ -15,8 +15,8 @@
 
 #define LENGTH 10.0 // x axis
 #define WIDTH 10.0  // z axis
-#define LENGTH_VERTEX_NUM 20
-#define WIDTH_VERTEX_NUM 20
+#define LENGTH_VERTEX_NUM 256
+#define WIDTH_VERTEX_NUM 256
 
 struct Vertex {
     glm::vec3 Position;
@@ -70,7 +70,7 @@ private:
         stbi_set_flip_vertically_on_load(true);
         unsigned char *heightmapData;
         int heightmapWidth, heightmapHeight, heightmapNrComponents;
-        heightmapData = stbi_load(heightPath, &heightmapWidth, &heightmapHeight, &heightmapNrComponents, 0);
+        heightmapData = stbi_load(heightPath, &heightmapWidth, &heightmapHeight, &heightmapNrComponents, 1);
         if (heightmapData == nullptr) {
             std::cerr << "Texture failed to load at path: " << heightPath << std::endl;
             return;
@@ -79,20 +79,25 @@ private:
 //        std::cout << "------------------" << std::endl;
 //        for (int i = 0; i < heightmapHeight; i++) {
 //            for (int j = 0; j < heightmapWidth; j++) {
-//                std::cout << (int)heightmapData[i * heightmapHeight + j] << " ";
+//                std::cout << (int)heightmapData[i * heightmapHeight + j] << ", ";
 //            }
 //            std:: cout << std::endl;
 //            std:: cout << std::endl;
 //        }
 //        std::cout << "------------------" << std::endl;
+//        for (int y = 0; y < height; y++) {
+//            for (int x = 0; x < width; x++) {
+//                histogram[pixels[x + y * height]]++;
+//            }
+//        }
 
         float lengthInterval = LENGTH / (float) (LENGTH_VERTEX_NUM-1);
         float widthInterval = WIDTH / (float) (WIDTH_VERTEX_NUM-1);
         for (int i = 0; i < LENGTH_VERTEX_NUM; i++) {
             for (int j = 0; j < WIDTH_VERTEX_NUM; j++) {
                 float x = lengthInterval * i, z = widthInterval * j;
-                float u = x / (LENGTH), v = 1.0 - z / (WIDTH);
-                int heightmap_u = u * heightmapHeight, heightmap_v = v * heightmapWidth;
+                float u = x / (LENGTH), v = z / (WIDTH);
+                int heightmap_u = u * (heightmapHeight-1), heightmap_v = v * (heightmapWidth-1);
                 float y = heightmapData[heightmap_u * heightmapHeight + heightmap_v] / 256.0 * 1.0;
 
                 Vertex vertex{glm::vec3(x, z, y), glm::vec2(u, v)};
@@ -112,17 +117,21 @@ private:
             }
         }
 
-        std::cout << "-----------vertices--------------" << std::endl;
-        for (int i = 0; i < vertices.size(); i++) {
-            std::cout << i << " " << vertices[i].Position.x << " " << vertices[i].Position.y << " " << vertices[i].Position.z << " "
-                      << vertices[i].TexCoords.x << " " << vertices[i].TexCoords.y << std::endl;
-        }
-        std::cout << "-----------indices---------------" << std::endl;
-        for (int i = 0; i < indices.size(); i++) {
-            if (i > 0 && i % 3 == 0) std::cout << std::endl;
-            if (i > 0 && i % 6 == 0) std::cout << std::endl;
-            std::cout << indices[i] << " ";
-        }
+//        std::cout << "-----------vertices--------------" << std::endl;
+//        for (int i = 0; i < vertices.size(); i++) {
+//            int heightmap_u = vertices[i].TexCoords.x * heightmapHeight, heightmap_v = vertices[i].TexCoords.y * heightmapWidth;
+//            float y = heightmapData[heightmap_u * heightmapHeight + heightmap_v];
+////            std::cout << i << " " << vertices[i].Position.x << " " << vertices[i].Position.y << " " << vertices[i].Position.z << " "
+////                      << y << std::endl;
+//            std::cout << i << " " << heightmap_u << " " << heightmap_v << " "
+//                      << y << std::endl;
+//        }
+//        std::cout << "-----------indices---------------" << std::endl;
+//        for (int i = 0; i < indices.size(); i++) {
+//            if (i > 0 && i % 3 == 0) std::cout << std::endl;
+//            if (i > 0 && i % 6 == 0) std::cout << std::endl;
+//            std::cout << indices[i] << " ";
+//        }
 
         stbi_image_free(heightmapData);
 
